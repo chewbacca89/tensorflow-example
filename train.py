4d9f1061410abbee7528ae1e8c1019b7f81b012a
chewbacca89 <klausmueller89@yahoo.de>
2022-05-30 15:30:48 +0200

    commit

--- train.py
+++ train.py
@@ -29,5 +29,5 @@ model.fit(x_train, y_train, epochs=5)
 
 model.evaluate(x_test, y_test, verbose=2)
 
-output_path = 'model.h5'
+output_path = valohai.outputs().path('model.h5')
 model.save(output_path)
